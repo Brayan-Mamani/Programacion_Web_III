1b3b423007c60ad925c77595b5755fc6749dddb4
Brayan-Mamani <135053177+Brayan-Mamani@users.noreply.github.com>
2025-11-12 09:00:42 -1200

    Delete model directory

--- model/productoModel.js
+++
@@ -1,7 +0,0 @@
-import {db} from '../config/db.js';
-export const AñadirProductos =async(producto)=>{
-  const {nombre,descripcion}= producto;
-  await db.query('INSERT INTO productos (nombre, precio, stock, categoria_id)VALUES (?, ?, ?, ?)',[nombre, precio, stock, categoria_id])
-  
-    return {message:"Se inserto correctamente"};
-}
